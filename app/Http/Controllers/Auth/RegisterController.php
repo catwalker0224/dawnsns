cba398b2aee95ddca82648f7731e333560b3571b
Masato Ueda <>
2022-03-27 20:48:54 +0900

    addedへ送るusernameをwithで記述

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -79,9 +79,9 @@ class RegisterController extends Controller
     public function register(Request $request){
         if($request->isMethod('post')){
             $data = $request->input();
-
+            $this->validator($data);
             $this->create($data);
-            return redirect('added');
+            return redirect('added')->with('username', $data['username']);
         }
         return view('auth.register');
     }
