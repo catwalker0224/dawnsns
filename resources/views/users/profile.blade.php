4e5e31a23b048f70c9542c6db720df3247f67598
Masato Ueda <>
2022-04-30 09:58:55 +0900

    マイプロフィールページのフォーム設置とCSS調整

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,7 +2,31 @@
 
 @section('content')
 
-プロフィールページ
-
-
+<!-- プロフィール編集フォーム -->
+<form action="users/profile" method="post" class="profile-edit">@csrf
+  <img class="loginIcon" src="images/{{ Auth::user()->images }}" alt="ログインユーザーアイコン">
+  <label class="username">UserName
+    <input type="text" name="username" value="{{ Auth::user()->username }}" class="username">
+  </label><br>
+  <label class="mail-address">MailAdress
+    <input type="email" name="mailAddress" value="{{ Auth::user()->mail }}" class="mail-address">
+  </label><br>
+  <label class="password">Password
+    <input type="password" name="password" value="{{ Auth::user()->password }}" class="password" readonly>
+  </label><br>
+  <label class="new-password">new Password
+    <input type="password" name="newPassword" class="new-password">
+  </label><br>
+  <label class="bio">Bio
+    <input type="text" name="bio" value="{{ Auth::user()->bio }}" class="bio">
+  </label><br>
+  <div class="button-area"></div>
+  <label class="icon-image">Icon Image
+    <div class="file-button">
+      <p class="choose-file">ファイルを選択</p>
+    </div>
+    <input type="file" name="iconImage" class="icon-image">
+  </label><br>
+  <button type="submit" class="profile-update">更　新</button>
+</form>
 @endsection
